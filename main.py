f9a127df47bd41609f3bad7ad92d542f02bd7c31
ESAKKIMUTHU2127 <88342613+ESAKKIMUTHU2127@users.noreply.github.com>
2021-08-02 23:37:22 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,9 @@
+for index in range(3):
+    basic.show_icon(IconNames.HEART)
+    basic.pause(1000)
+    basic.clear_screen()
+    basic.show_number(0)
+    basic.pause(1000)
+    basic.show_string("I am esaki")
+    basic.pause(2000)
+    break
---
+++ main.ts
@@ -0,0 +1,10 @@
+for (let index = 0; index < 3; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.showNumber(0)
+    basic.pause(1000)
+    basic.showString("I am esaki")
+    basic.pause(2000)
+    break
+}
